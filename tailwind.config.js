71e913c9bc414bc9bc50ae5699d2a93bb377c673
taylorhoyt <31395207+taylorhoyt@users.noreply.github.com>
2024-04-03 16:33:28 -0500

    Completed base site

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,15 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    fontFamily: {
+      'roboto': ['Roboto', 'sans-serif'],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        'theme-navy': '#070F2B',
+        'theme-dark-blue': '#1B1A55',
+        'theme-stone-blue': '#535C91',
+        'theme-lavender': '#9290C3',
       },
     },
   },
